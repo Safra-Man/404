8ce840bf67ac7d4f9fcc45e8c8703d25639d996b
Victor Brayner <victor.brayner@contasimples.com>
2025-11-21 14:43:32 -0300

    feat: adiciona componente Footer

--- FateConnect/Front/src/app/shared/ui/footer/footer.component.ts
+++ FateConnect/Front/src/app/shared/ui/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule, MatDividerModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faLocationDot = faLocationDot;
 }
