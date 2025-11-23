9ce97f41cd98f92d6c703c978e208a3304698458
Victor Brayner <victor.brayner@contasimples.com>
2025-11-23 18:37:31 -0300

    feat: implementa carregamento para a tela de caronas

--- FateConnect/Front/src/app/pages/caronas/screens/buscar-carona/buscar-carona.component.ts
+++ FateConnect/Front/src/app/pages/caronas/screens/buscar-carona/buscar-carona.component.ts
@@ -3,31 +3,38 @@ import { Component, inject } from '@angular/core';
 import { CaronaCardComponent } from '../../components/carona-card/carona-card.component';
 import { Carona } from '../../models/carona.model';
 import { CaronaService } from '../../services/carona.service';
+import { finalize } from 'rxjs/operators';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-buscar-carona',
-  imports: [CommonModule, CaronaCardComponent],
+  imports: [CommonModule, CaronaCardComponent, MatProgressSpinnerModule],
   templateUrl: './buscar-carona.component.html',
-  styleUrl: './buscar-carona.component.scss'
+  styleUrl: './buscar-carona.component.scss',
 })
 export class BuscarCaronaComponent {
   private caronaService = inject(CaronaService);
 
   listaCaronas: Carona[] = [];
+  isLoading = false;
 
   ngOnInit(): void {
     this.buscarDados();
   }
 
   buscarDados() {
-    this.caronaService.listarCaronas().subscribe({
-      next: (dadosApi) => {
-        console.log(dadosApi);
-        this.listaCaronas = dadosApi;
-      },
-      error: (erro) => {
-        console.error('Erro ao buscar caronas:', erro);
-      }
-    });
+    this.isLoading = true;
+    this.caronaService
+      .listarCaronas()
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe({
+        next: (dadosApi) => {
+          console.log(dadosApi);
+          this.listaCaronas = dadosApi;
+        },
+        error: (erro) => {
+          console.error('Erro ao buscar caronas:', erro);
+        },
+      });
   }
 }
