254b7a1ccaf208a2144807a144870900852e63cc
Victor Brayner <victor.brayner@contasimples.com>
2025-11-23 18:42:41 -0300

    feat: implementa tela inicial da aplicação (#54)

--- FateConnect/Front/src/app/pages/menu/menu.component.ts
+++ FateConnect/Front/src/app/pages/menu/menu.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
-
+  faSearch = faSearch;
+  faCar = faCar;
 }
