c9ed8dd5d52bfb59249d42cda0b7d2d7f9f4d3d4
Victor Brayner <victor.brayner@contasimples.com>
2025-11-23 19:36:13 -0300

    feat: implementa exclusão de caronas

--- FateConnect/Front/src/app/pages/caronas/screens/buscar-carona/buscar-carona.component.ts
+++ FateConnect/Front/src/app/pages/caronas/screens/buscar-carona/buscar-carona.component.ts
@@ -5,15 +5,17 @@ import { Carona } from '../../models/carona.model';
 import { CaronaService } from '../../services/carona.service';
 import { finalize } from 'rxjs/operators';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-buscar-carona',
-  imports: [CommonModule, CaronaCardComponent, MatProgressSpinnerModule],
+  imports: [CommonModule, CaronaCardComponent, MatProgressSpinnerModule, MatSnackBarModule],
   templateUrl: './buscar-carona.component.html',
   styleUrl: './buscar-carona.component.scss',
 })
 export class BuscarCaronaComponent {
   private caronaService = inject(CaronaService);
+  private snackBar = inject(MatSnackBar);
 
   listaCaronas: Carona[] = [];
   isLoading = false;
@@ -34,6 +36,30 @@ export class BuscarCaronaComponent {
         },
         error: (erro) => {
           console.error('Erro ao buscar caronas:', erro);
+        },
+      });
+  }
+
+  onExcluir(carona: Carona) {
+    this.isLoading = true;
+    this.caronaService
+      .excluirCarona(carona.id)
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe({
+        next: () => {
+          this.listaCaronas = this.listaCaronas.filter((c) => c.id !== carona.id);
+          this.snackBar.open('Carona excluída com sucesso.', 'OK', {
+            duration: 3000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-success'],
+          });
+        },
+        error: () => {
+          this.snackBar.open('Erro ao excluir a carona. Tente novamente.', 'Fechar', {
+            duration: 4000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-error'],
+          });
         },
       });
   }
