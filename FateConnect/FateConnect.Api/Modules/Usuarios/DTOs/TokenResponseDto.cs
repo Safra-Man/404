441be3dc866eb4f187c62f6dcf0e4932bcd94ebd
Matheus Lauton <mlautonsantana@gmail.com>
2026-05-07 00:01:36 -0300

    Delete FateConnect/FateConnect.Api directory

--- FateConnect/FateConnect.Api/Modules/Usuarios/DTOs/TokenResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace FateConnect.Api.Modules.Usuarios.DTOs
-{
-    public class TokenResponseDto
-    {
-        public string Token { get; set; } = string.Empty;
-        public string NomeCompleto { get; set; } = string.Empty;
-    }
-}
