21c0551e968830cc9117a67ad4ae39a43e8a097d
Matheus Lauton <mlautonsantana@gmail.com>
2026-05-07 00:05:20 -0300

    Delete FateConnect/FateConnect.Api directory

--- FateConnect/FateConnect.Api/Modules/Auth/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using FateConnect.Api.Modules.Usuarios.DTOs;
-
-namespace FateConnect.Api.Modules.Auth.Interfaces
-{
-    public interface IAuthService
-    {
-        Task<TokenResponseDto> LoginAsync(LoginDto dto);
-    }
-}
--- FateConnect/FateConnect.Api/Modules/Auth/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-using FateConnect.Api.Modules.Usuarios;
-
-namespace FateConnect.Api.Modules.Auth.Interfaces
-{
-    public interface ITokenService
-    {
-        string GerarJwtToken(Usuario usuario);
-    }
-}
